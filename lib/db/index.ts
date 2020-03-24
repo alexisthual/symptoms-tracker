edce512a22e4d6ece328d6a041808e7bd3ed9e9e
alexis <alexisthual@gmail.com>
2020-03-24 01:36:15 +0100

    Add ssl configuration for prod postgres database

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,22 @@
 // Taken from:
 // https://github.com/glennreyes/now-typeorm-typegraphql-starter/blob/master/db/index.ts
 
-// import 'reflect-metadata';
+import "reflect-metadata";
 import { ConnectionOptions, getConnectionManager } from "typeorm";
 
+const sslOptions =
+  process.env.NODE_ENV === "production" ||
+  (process.env.NODE_ENV as string) === "stage"
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+          ca: process.env.POSTGRES_SSL_CERT
+        }
+      }
+    : {};
+
 const connectionOptions: ConnectionOptions = {
+  ...sslOptions,
   type: "postgres",
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
