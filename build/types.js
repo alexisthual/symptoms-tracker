378624c12c377ae09e580c97a7337f550b8fdebf
alexis <alexisthual@gmail.com>
2020-03-24 20:13:58 +0100

    Build AddCough migration

---
+++ build/types.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.__esModule = true;
+var ageCategories;
+(function(ageCategories) {
+  ageCategories["CHILD"] = "0";
+  ageCategories["ADULT"] = "1";
+  ageCategories["SENIOR"] = "2";
+  ageCategories["AGING"] = "3";
+  ageCategories["ELDERLY"] = "4"; // 75+
+})((ageCategories = exports.ageCategories || (exports.ageCategories = {})));
+var headacheStates;
+(function(headacheStates) {
+  headacheStates["YES"] = "YES";
+  headacheStates["NO"] = "NO";
+})((headacheStates = exports.headacheStates || (exports.headacheStates = {})));
+var feverStates;
+(function(feverStates) {
+  feverStates["HIGH"] = "HIGH";
+  feverStates["MILD"] = "MILD";
+  feverStates["NO"] = "NO";
+})((feverStates = exports.feverStates || (exports.feverStates = {})));
+var coughStates;
+(function(coughStates) {
+  coughStates["DRY"] = "DRY";
+  coughStates["WET"] = "WET";
+  coughStates["NO"] = "NO";
+})((coughStates = exports.coughStates || (exports.coughStates = {})));
+var breathingStates;
+(function(breathingStates) {
+  breathingStates["HIGH"] = "HIGH";
+  breathingStates["MILD"] = "MILD";
+  breathingStates["NO"] = "NO";
+})(
+  (breathingStates = exports.breathingStates || (exports.breathingStates = {}))
+);
