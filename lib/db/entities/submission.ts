fc2f5258cd5991cef951e57a2accdee3c6589009
alexis <alexisthual@gmail.com>
2020-03-25 01:20:10 +0100

    Modify submission entry to fit new questions

--- lib/db/entities/submission.ts
+++ lib/db/entities/submission.ts
@@ -3,7 +3,17 @@ import {
   headacheStates,
   feverStates,
   coughStates,
-  breathingStates
+  breathingStates,
+  throatStates,
+  diarrheaStates,
+  feedingStates,
+  hypertensionStates,
+  diabetesStates,
+  cancerStates,
+  pneumoStates,
+  dialyseStates,
+  liverStates,
+  immunoStates
 } from "../../types";
 
 @Entity()
@@ -32,6 +42,42 @@ export class Submission {
     nullable: true
   })
   headacheSince: number;
+
+  @Column({
+    type: "enum",
+    enum: throatStates
+  })
+  throat: throatStates;
+
+  @Column({
+    type: "smallint",
+    nullable: true
+  })
+  throatSince: number;
+
+  @Column({
+    type: "enum",
+    enum: diarrheaStates
+  })
+  diarrhea: diarrheaStates;
+
+  @Column({
+    type: "smallint",
+    nullable: true
+  })
+  diarrheaSince: number;
+
+  @Column({
+    type: "enum",
+    enum: feedingStates
+  })
+  feeding: feedingStates;
+
+  @Column({
+    type: "smallint",
+    nullable: true
+  })
+  feedingSince: number;
 
   @Column({
     type: "enum",
@@ -69,4 +115,71 @@ export class Submission {
     nullable: true
   })
   breathingSince: number;
+
+  @Column({
+    type: "integer",
+    nullable: true
+  })
+  height: number;
+
+  @Column({
+    type: "integer",
+    nullable: true
+  })
+  weight: number;
+
+  @Column({
+    type: "enum",
+    enum: hypertensionStates,
+    nullable: true
+  })
+  hypertension: hypertensionStates;
+
+  @Column({
+    type: "enum",
+    enum: diabetesStates,
+    nullable: true
+  })
+  diabetes: diabetesStates;
+
+  @Column({
+    type: "enum",
+    enum: cancerStates,
+    nullable: true
+  })
+  cancer: cancerStates;
+
+  @Column({
+    type: "enum",
+    enum: pneumoStates,
+    nullable: true
+  })
+  pneumo: pneumoStates;
+
+  @Column({
+    type: "enum",
+    enum: dialyseStates,
+    nullable: true
+  })
+  dialyse: dialyseStates;
+
+  @Column({
+    type: "enum",
+    enum: liverStates,
+    nullable: true
+  })
+  liver: liverStates;
+
+  @Column({
+    type: "enum",
+    enum: immunoStates,
+    nullable: true
+  })
+  immuno: immunoStates;
+
+  @Column({
+    type: "integer",
+    nullable: true
+  })
+  confined: number;
 }
