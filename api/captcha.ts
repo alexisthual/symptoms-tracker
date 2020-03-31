912dfce24a2b9065d95886e085dd63348854d1f8
alexis <alexisthual@gmail.com>
2020-03-31 15:25:31 +0200

    Add endpoint generating captcha

---
+++ api/captcha.ts
@@ -0,0 +1,40 @@
+import { NowRequest, NowResponse } from "@now/node";
+import svgCaptcha from "svg-captcha";
+
+import { connect } from "../lib/db";
+import { Captcha } from "../lib/db/entities/captcha";
+
+module.exports = async (req: NowRequest, res: NowResponse) => {
+  if (req.method === "GET") {
+    const connection = await connect();
+    var captcha = svgCaptcha.create({
+      size: 4,
+      ignoreChars: "0oO1iLl",
+      noise: 3,
+      color: true
+    });
+
+    console.log(captcha.text);
+
+    const captchaRepository = connection.getRepository(Captcha);
+    captchaRepository
+      .save({
+        text: captcha.text
+      })
+      .then((entry: Captcha) => {
+        res.status(200).json({
+          id: entry.id,
+          data: captcha.data
+        });
+      })
+      .catch((error: any) => {
+        res.status(500).json({
+          error
+        });
+      });
+  } else {
+    res.status(501).json({
+      message: "Unsupported request type."
+    });
+  }
+};
