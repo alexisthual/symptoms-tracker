392dde83505d8eceb3e55a72fe936742e1e746a5
alexis <alexisthual@gmail.com>
2020-04-01 14:34:33 +0200

    Add meta data and image for social media

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,9 +7,27 @@ class MyDocument extends Document {
   }
 
   render() {
+    const url = "https://coronastatus.fr";
+    const name = "Corona Status";
+    const title = "Questionnaire pour les populations confinées";
+    const description =
+      "Aidez-nous à estimer et prédire la propagation du COVID-19 en France en remplissant ce questionnaire. Votre réponse est utile même si vous n'êtes pas malade.";
+
     return (
       <Html>
         <Head>
+          <meta property="og:site_name" content={name} />
+          <meta property="og:url" content={url} />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={`${url}/social-media.png`} />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:image" content={`${url}/social-media.png`} />
+
           <link
             rel="apple-touch-icon"
             sizes="57x57"
