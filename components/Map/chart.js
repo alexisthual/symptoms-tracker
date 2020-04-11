4c2cd25d6820c80896dc9d8c81aab32562e8945a
Loick Le Digabel <loick.ledigabel@gmail.com>
2020-04-11 22:57:50 +0200

    Update the map hover state

--- components/Map/chart.js
+++ components/Map/chart.js
@@ -1,77 +1,77 @@
-import { geoPath, geoConicConformal } from 'd3-geo';
-import { scaleLinear, scaleQuantile } from 'd3-scale';
-import { axisLeft } from 'd3-axis';
-import { select, selectAll, mouse } from 'd3-selection';
-import { max, min, range } from 'd3-array';
+import { geoPath, geoConicConformal } from "d3-geo";
+import { scaleLinear, scaleQuantile } from "d3-scale";
+import { axisLeft } from "d3-axis";
+import { select, selectAll, mouse } from "d3-selection";
+import { max, min, range } from "d3-array";
 
 const colors = [
-  '#FFEBE1', // Not enough answers
-  '#FFD0BA',
-  '#FEBC9C',
-  '#FFA87F',
-  '#FF8D58',
-  '#FF8044',
-  '#FE6C27',
-  '#FF5200', // Enough answers
+  "#FFEBE1", // Not enough answers
+  "#FFD0BA",
+  "#FEBC9C",
+  "#FFA87F",
+  "#FF8D58",
+  "#FF8044",
+  "#FE6C27",
+  "#FF5200" // Enough answers
 ];
 const LEGEND_HEIGHT = 300;
 const LEGEND_SIZE = Math.ceil(LEGEND_HEIGHT / colors.length);
 
-const REGION_HOVER_COLOR = '#333';
+const REGION_HOVER_COLOR = "#333";
 
 function addTooltip(svg) {
   var tooltip = svg
-    .append('g') // Group for the whole tooltip
-    .attr('id', 'tooltip')
-    .style('display', 'none');
+    .append("g") // Group for the whole tooltip
+    .attr("id", "tooltip")
+    .style("display", "none");
 
   tooltip
-    .append('polyline') // The rectangle containing the text, it is 210px width and 60 height
-    .attr('points', '0,0 210,0 210,60 0,60 0,0')
-    .style('fill', '#222b1d')
-    .style('stroke', 'black')
-    .style('opacity', '0.9')
-    .style('stroke-width', '1')
-    .style('padding', '1em');
+    .append("polyline") // The rectangle containing the text, it is 210px width and 60 height
+    .attr("points", "0,0 210,0 210,60 0,60 0,0")
+    .style("fill", "#222b1d")
+    .style("stroke", "black")
+    .style("opacity", "0.9")
+    .style("stroke-width", "1")
+    .style("padding", "1em");
 
   tooltip
-    .append('line') // A line inserted between country name and score
-    .attr('x1', 40)
-    .attr('y1', 25)
-    .attr('x2', 160)
-    .attr('y2', 25)
-    .style('stroke', '#929292')
-    .style('stroke-width', '0.5')
-    .attr('transform', 'translate(0, 5)');
+    .append("line") // A line inserted between country name and score
+    .attr("x1", 40)
+    .attr("y1", 25)
+    .attr("x2", 160)
+    .attr("y2", 25)
+    .style("stroke", "#929292")
+    .style("stroke-width", "0.5")
+    .attr("transform", "translate(0, 5)");
 
   var text = tooltip
-    .append('text') // Text that will contain all tspan (used for multilines)
-    .style('font-size', '13px')
-    .style('fill', '#c1d3b8')
-    .attr('transform', 'translate(0, 20)');
+    .append("text") // Text that will contain all tspan (used for multilines)
+    .style("font-size", "13px")
+    .style("fill", "#c1d3b8")
+    .attr("transform", "translate(0, 20)");
 
   text
-    .append('tspan') // Country name udpated by its id
-    .attr('x', 105) // ie, tooltip width / 2
-    .attr('y', 0)
-    .attr('id', 'tooltip-country')
-    .attr('text-anchor', 'middle')
-    .style('font-weight', '600')
-    .style('font-size', '16px');
+    .append("tspan") // Country name udpated by its id
+    .attr("x", 105) // ie, tooltip width / 2
+    .attr("y", 0)
+    .attr("id", "tooltip-country")
+    .attr("text-anchor", "middle")
+    .style("font-weight", "600")
+    .style("font-size", "16px");
 
   text
-    .append('tspan') // Fixed text
-    .attr('x', 105) // ie, tooltip width / 2
-    .attr('y', 30)
-    .attr('text-anchor', 'middle')
-    .style('fill', '929292')
-    .text('Total : ');
+    .append("tspan") // Fixed text
+    .attr("x", 105) // ie, tooltip width / 2
+    .attr("y", 30)
+    .attr("text-anchor", "middle")
+    .style("fill", "929292")
+    .text("Total : ");
 
   text
-    .append('tspan') // Score udpated by its id
-    .attr('id', 'tooltip-score')
-    .style('fill', '#c1d3b8')
-    .style('font-weight', 'bold');
+    .append("tspan") // Score udpated by its id
+    .attr("id", "tooltip-score")
+    .style("fill", "#c1d3b8")
+    .style("font-weight", "bold");
 
   return tooltip;
 }
@@ -95,40 +95,40 @@ export const createChart = async ({ id, geojson, csv }) => {
   path.projection(projection);
 
   const svg = select(`#${id}`)
-    .append('svg')
-    .attr('id', 'svg')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('class', 'Blues');
+    .append("svg")
+    .attr("id", "svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "Blues");
 
-  const deps = svg.append('g');
+  const deps = svg.append("g");
 
   // Draw regions
   deps
-    .selectAll('path')
+    .selectAll("path")
     .data(geojson.features)
     .enter()
-    .append('path')
-    .attr('id', d => `code-${d.properties.code}`)
-    .attr('d', path);
+    .append("path")
+    .attr("id", d => `code-${d.properties.code}`)
+    .attr("d", path);
 
   var quantile = scaleQuantile()
     .domain([0, max(csv, e => e.TOTAL)])
     .range(range(colors.length));
 
   // Create the legend
-  var legend = svg.append('g').attr('transform', 'translate(550, 100)');
+  var legend = svg.append("g").attr("transform", "translate(550, 100)");
 
   legend
     .selectAll()
     .data(range(colors.length))
     .enter()
-    .append('svg:rect')
-    .attr('height', LEGEND_SIZE + 'px')
-    .attr('width', LEGEND_SIZE + 'px')
-    .attr('x', 5)
-    .attr('y', d => d * LEGEND_SIZE)
-    .style('fill', d => colors[d]);
+    .append("svg:rect")
+    .attr("height", LEGEND_SIZE + "px")
+    .attr("width", LEGEND_SIZE + "px")
+    .attr("x", 5)
+    .attr("y", d => d * LEGEND_SIZE)
+    .style("fill", d => colors[d]);
 
   // Create the legend scale
   const minLegend = min(csv, e => e.TOTAL);
@@ -138,8 +138,8 @@ export const createChart = async ({ id, geojson, csv }) => {
     .range([0, colors.length * LEGEND_SIZE]);
 
   const legendAxis = legend
-    .append('g')
-    .attr('class', 'axis')
+    .append("g")
+    .attr("class", "axis")
     .call(axisLeft(legendScale));
 
   const tooltip = addTooltip(svg);
@@ -150,31 +150,32 @@ export const createChart = async ({ id, geojson, csv }) => {
     const answersPercentage = TOTAL;
     const regionColor = colors[quantile(answersPercentage)];
     region
-      .style('fill', () => regionColor)
-      .attr('class', () => 'q' + quantile(answersPercentage) + '-9');
+      .style("fill", () => regionColor)
+      .style("stroke-width", "0.5")
+      .attr("class", () => "q" + quantile(answersPercentage) + "-9");
 
-    region.on('mouseover', function(d) {
-      console.log(e);
-      region.style('fill', REGION_HOVER_COLOR);
-      tooltip.style('display', 'block');
-      tooltip.select('#tooltip-country').text(REGION);
-      tooltip.select('#tooltip-score').text(answersPercentage);
+    region.on("mouseover", function(d) {
+      region.style("stroke", REGION_HOVER_COLOR);
+      tooltip.style("display", "block");
+      tooltip.select("#tooltip-country").text(REGION);
+      tooltip.select("#tooltip-score").text(answersPercentage);
     });
 
-    region.on('mouseout', function() {
-      region.style('fill', d => regionColor);
-      tooltip.style('display', 'none');
-      legend.select('#cursor').style('display', 'none');
+    region.on("mouseout", function() {
+      region.style("fill", d => regionColor);
+      region.style("stroke", "none");
+      tooltip.style("display", "none");
+      legend.select("#cursor").style("display", "none");
     });
 
-    region.on('mousemove', function() {
+    region.on("mousemove", function() {
       var [x, y] = mouse(this);
-      tooltip.attr('transform', 'translate(' + x + ',' + (y - 75) + ')');
+      tooltip.attr("transform", "translate(" + x + "," + (y - 75) + ")");
     });
   });
 
-  select('select').on('change', function() {
-    selectAll('svg').attr('class', this.value);
+  select("select").on("change", function() {
+    selectAll("svg").attr("class", this.value);
   });
 
   return Promise.resolve();
